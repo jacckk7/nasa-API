8147ca031e256782179f6fcbb15d539fa5c3a0c0
BRENO COSTA AVELINO LIMA <breno.lima@equatorialenergia.com.br>
2026-01-08 11:12:55 -0300

    style: changed not found page style

--- src/pages/notfound.tsx
+++ src/pages/notfound.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold text-red-500">
+    <div className="p-6 h-screen flex items-center">
+      <h1 className="text-center text-3xl font-bold text-red-500">
         Página não encontrada
       </h1>
     </div>
